d266e8c26e690a6fa96b192c57191036aa7e3abf
joseaam2020 <joseaam2019@hotmail.com>
2023-08-23 15:25:36 -0600

    Arreglos en orden de main

--- main.py
+++ main.py
@@ -5,36 +5,38 @@ def printParidad():
 
 #Creacion ventana
 root = tk.Tk()
-root.geometry("500x500")
 root.title("Proyecto 1 Diseño Logico")
 
-#Creacion Labels
+#Creacion ingreso de numero
 Lingreso = tk.Label(root,text ="Ingrese numero binario: ")
-Lbinario = tk.Label(root,text ="Binario: ")
-Loctal = tk.Label(root,text ="Octal: ")
-Ldecimal = tk.Label(root,text ="Decimal: ")
-LNZRI = tk.Label(root,text ="Señal NRZI: ")
-lsignal = tk.Label(root, text="[insertar imagen de señal]")
-Lparidad = tk.Label(root,text ="Paridad: ")
-Lingreso.grid(column=0,row=0)
-Lbinario.grid(column=0,row=1)
-Loctal.grid(column=0,row=2)
-Ldecimal.grid(column=0,row=3)
-LNZRI.grid(column=0,row=4,columnspan=2)
-lsignal.grid(column=0,row=5,columnspan=2)
-Lparidad.grid(column=0,row=6,rowspan=2)
-
-#Creacion entries
 numeroBinarioHexadecimal = tk.Entry(root)
+Bingreso = tk.Button(root,text="Ingresar")
+Lingreso.grid(column=0,row=0)
 numeroBinarioHexadecimal.grid(column=1,row=0)
+Bingreso.grid(column=2,row=0)
+
+#Creacion tabla 1
+T1titulo = tk.Label(root,text ="Tabla 1. Conversión de numero a Binario, Octal y decimal",anchor="center")
+T1titulo.grid(column=0,row=1,columnspan=3)
+
+
+#Creacion señal
+LNZRI = tk.Label(root,text ="Señal NRZI: ", anchor="center")
+lsignal = tk.Label(root, text="[insertar imagen de señal]",anchor="center")
+LNZRI.grid(column=0,row=4,columnspan=3)
+lsignal.grid(column=0,row=5,columnspan=3)
+
+
 
 #Opciones paridad
+Lparidad = tk.Label(root,text ="Paridad: ")
+Lparidad.grid(column=0,row=6,rowspan=2)
 paridad = tk.IntVar()
 paridad.set(2)
 Rparpar = tk.Radiobutton(root, text="Paridad par", variable=paridad, value=2,command=printParidad)
 Rparimpar = tk.Radiobutton(root, text="Paridad impar", variable=paridad, value=1,command=printParidad)
-Rparpar.grid(column=1,row=6)
-Rparimpar.grid(column=1,row=7)
+Rparpar.grid(column=1,row=6,columnspan=2)
+Rparimpar.grid(column=1,row=7,columnspan=2)
 
 
 root.mainloop()
